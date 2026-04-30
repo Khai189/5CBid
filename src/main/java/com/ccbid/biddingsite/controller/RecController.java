21d81b514cd94203e276f35db83c49a0c5bc7ddc
Khai189 <khmr2024@mymail.pomona.edu>
2026-04-30 15:19:41 -0700

    Adding new rec controller and rec service additions for a graph

---
+++ src/main/java/com/ccbid/biddingsite/controller/RecController.java
@@ -0,0 +1,5 @@
+package com.ccbid.biddingsite.controller;
+
+public class RecController {
+    
+}
--- src/main/java/com/ccbid/biddingsite/service/RecService.java
+++ src/main/java/com/ccbid/biddingsite/service/RecService.java
@@ -1,11 +1,27 @@
 package com.ccbid.biddingsite.service;
 
 import org.springframework.stereotype.Service;
+import com.ccbid.biddingsite.dataStructures.ItemGraph;
 import com.ccbid.biddingsite.models.BidItem;
+import com.ccbid.biddingsite.repository.ItemRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Service
 public class RecService {
+
+    @Autowired
+    private ItemRepo itemRepo;
+
+    @Autowired
+    private BidService bidService;
+
+
+
+    ItemGraph<BidItem> itemGraph = new ItemGraph<>();
+
+    
+
     
     
 }
